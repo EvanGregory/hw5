7119739cd4aeb066c5175e5adf85fea1e9d62e33
Evan Gregory <ejgregor@usc.edu>
2023-04-06 02:28:13 -0700

    wordle v1

--- wordle.cpp
+++ wordle.cpp
@@ -22,7 +22,53 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+  std::set<std::string> correctWords = fillAllLetters(in, floating);
+  return removeBadWords(correctWords, dict);
 }
 
 // Define any helper functions here
+
+std::set<std::string> fillAllLetters(const std::string& s, const std::string& floating, int pos = 0)
+{
+  std::set<std::string> currSet{};
+  if (s.empty() || pos >= s.size())
+    return currSet;
+  //(s.substr(0, pos) + letter + s.substr(pos))
+  if (s[pos] != '-')
+  {
+    return fillAllLetters(s, floating, pos + 1);
+  }
+  else
+  {
+    if (floating.size() >= s.size() - pos)
+    {
+      for (char letter : floating)
+      {
+        std::set<std::string> subSet = fillAllLetters(s.substr(0, pos) + letter + s.substr(pos), floating, pos + 1);
+        currSet.insert(subSet.begin(), subSet.end());
+      }
+    }
+    else
+    {
+      for (char letter = 'a'; letter <= 'z'; letter++)
+      {
+        std::set<std::string> subSet = fillAllLetters(s.substr(0, pos) + letter + s.substr(pos), floating, pos + 1);
+        currSet.insert(subSet.begin(), subSet.end());
+      }
+    }
+  }
+  return currSet;
+}
+
+std::set<std::string> removeBadWords(const std::set<std::string>& in, const std::set<std::string>& dict)
+{
+  std::set<std::string> realWords;
+  for (std::string word : in)
+  {
+    if (dict.find(word) != dict.end())
+    {
+      realWords.insert(word);
+    }
+  }
+  return realWords;
+}

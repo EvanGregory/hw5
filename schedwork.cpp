73ba668e1da6c00269e91d67e0d738d534cb3630
Evan Gregory <ejgregor@usc.edu>
2023-04-07 23:51:39 -0700

    it works maybe???

--- schedwork.cpp
+++ schedwork.cpp
@@ -23,10 +23,13 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 // Add prototypes for any helper functions here
 std::pair<bool, DailySchedule> recurseFillSched(const AvailabilityMatrix& avail,
   const size_t dailyNeed,
-  const size_t maxShifts,
+  const size_t maxShifts, const DailySchedule& workerCombos,
   DailySchedule sched, const size_t pos);
 
 Worker_T findWorker(const AvailabilityMatrix& avail, size_t day, size_t& pos);
+
+void workerCombinations(std::vector<Worker_T> arr, int len, int startPosition, std::vector<Worker_T>& combo, DailySchedule& result);
+
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -41,7 +44,23 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-    pair<bool, DailySchedule> final = recurseFillSched(avail, 2, maxShifts, sched, 0);
+    vector<Worker_T> combo;
+    vector<Worker_T> workerIDs;
+    DailySchedule workerCombos;
+    //initialize combo
+    for (int i = 0; i < dailyNeed; i++)
+    {
+      combo.push_back(INVALID_ID);
+    }
+    //initialize ids
+    for (int i = 0; i < avail[0].size(); i++)
+    {
+      workerIDs.push_back(i);
+    }
+
+    //generate all possible combinations
+    workerCombinations(workerIDs, dailyNeed, 0, combo, workerCombos);
+    pair<bool, DailySchedule> final = recurseFillSched(avail, dailyNeed, maxShifts, workerCombos, sched, 0);
     if (!final.first)
       return false;
     sched = final.second;
@@ -50,56 +69,85 @@ bool schedule(
 
 std::pair<bool, DailySchedule> recurseFillSched(const AvailabilityMatrix& avail,
   const size_t dailyNeed,
-  const size_t maxShifts,
+  const size_t maxShifts, 
+  const DailySchedule& workerCombos,
   DailySchedule sched, const size_t pos)
 {
-  //if pos is at the end, return true, return sched
-  if (avail.size() >= pos)
+
+  //checking if sched fails
+  {
+    //checks if previous day uses only available workers
+    if (pos > 0)
+      for (Worker_T worker : sched[pos - 1])
+        if (!avail[pos - 1][worker])
+          return make_pair(false, sched);
+
+    //checks if any worker is overworked
+    vector<int> daysWorked;
+    for (bool t : avail[0])
+      daysWorked.push_back(0);
+
+    for (int day = 0; day < pos; day++)
+    {
+      for (int i = 0; i < dailyNeed; i++)
+      {
+        Worker_T worker = sched[day][i];
+        daysWorked[worker]++;
+        if (daysWorked[worker] > maxShifts)
+          return make_pair(false, sched);
+      }
+    }
+    //could add more precise backtracing checks here, look ahead to see if the current state is impossible
+  }
+
+  //reached the end, return true
+  if (avail.size() <= pos)
+  {
     return make_pair(true, sched);
-
-  {//make sure sched has room on the next line
-    vector<Worker_T> t;
-    sched.push_back(t);
   }
 
-  //if current sched will not work, return false, dont care about state of return sched
-  bool schedWorks = true;
-  vector<int> daysWorked;
-  for (bool t : avail[0])
-    daysWorked.push_back(0);
-
-  for (int day = 0; day <= pos; day++)
+  for (vector<Worker_T> attempt : workerCombos)
   {
-    for (int i = 0; i < sched[day].size(); i++)
-    {
-      Worker_T worker = sched[day][i];
-      daysWorked[worker]++;
-      if (daysWorked[worker] > maxShifts)
-        schedWorks = false;
-    }
-  }
-  //could add more precise backtracing checks here, look ahead to see if the current state is impossible
-  if (!schedWorks)
-    return make_pair(false, sched);
-
-
-  while(true)
-  {
-    size_t workerPos = 0;
     DailySchedule tempSched = sched;
-    //pushback a worker dailyNeed times
-    for (size_t num = 1; num <= dailyNeed; num++)
-    {
-      Worker_T currWorker = findWorker(avail, pos, workerPos);
-      if (currWorker == INVALID_ID)
-        return make_pair(false, sched);
-      tempSched[pos].push_back(currWorker);
-    }
-    std::pair<bool, DailySchedule> attempt = recurseFillSched(avail, dailyNeed, maxShifts, tempSched, pos + 1);
-    if (attempt.first)
-      return attempt;
+    tempSched.push_back(attempt);
+    std::pair<bool, DailySchedule> pairAttempt = recurseFillSched(avail, dailyNeed, maxShifts, workerCombos, tempSched, pos + 1);
+    if (pairAttempt.first)
+      return pairAttempt;
   }
   return make_pair(false, sched);
+
+  //vector<vector<Worker_T>> attemptList;
+  //while(true)
+  //{
+  //  size_t workerPos = 0;
+  //  DailySchedule tempSched = sched;
+  //  //pushback a worker dailyNeed times
+  //  for (size_t num = 1; num <= dailyNeed; num++)
+  //  {
+  //    
+  //    vector<Worker_T> superTemp;
+  //    Worker_T currWorker;
+  //    //if we have already tried that worker combination, try again
+  //    do { 
+  //      superTemp = tempSched[pos];
+  //      currWorker = findWorker(avail, pos, workerPos); 
+  //      superTemp.push_back(currWorker);
+  //    }
+  //    while (std::find(attemptList.begin(), attemptList.end(), superTemp) != attemptList.end());
+  //    if (currWorker == INVALID_ID)
+  //    {
+  //      //find last available worker
+  //      return make_pair(false, sched);
+  //    }
+  //    tempSched[pos].push_back(currWorker);
+
+  //  }
+  //  std::pair<bool, DailySchedule> attempt = recurseFillSched(avail, dailyNeed, maxShifts, tempSched, pos + 1);
+  //  if (attempt.first)
+  //    return attempt;
+  //  attemptList.push_back(attempt.second[pos]);
+  //}
+  //return make_pair(false, sched);
 
   //for each possible combination of available workers today, recursive call passing down sched with one more day filled in. pass pos + 1 for next day
 
@@ -134,4 +182,24 @@ Worker_T findWorker(const AvailabilityMatrix& avail, size_t day, size_t& pos)
   }
 
   return INVALID_ID;
+}
+
+
+
+// ex. combinations2(arr, 3, 0, new String[3]);
+
+void workerCombinations(vector<Worker_T> arr, int len, int startPosition, vector<Worker_T>& combo, DailySchedule& result) 
+{
+  if (len == 0)
+  {
+    result.push_back(combo);
+    return;
+  }
+
+  for (int i = startPosition; i <= arr.size() - len; i++) 
+  {
+
+    combo[combo.size() - len] = arr[i];
+    workerCombinations(arr, len - 1, i + 1, combo, result);
+  }
 }
